f349661f84bafa9f48f30228dd5d60a161d80df5
MOADSOUFIANE <joseluis45re534@gmail.com>
2026-01-23 23:24:58 +0100

    fix: resolve syntax error in routes and enable D1 storage

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,4 +1,11 @@
 import { handle } from 'hono/cloudflare-pages';
 import app from '../../server/routes_hono';
+import { initializeStorage } from '../../server/storage';
+import { D1Storage } from '../../server/storage_d1';
 
-export const onRequest = handle(app);
+export const onRequest = async (context: any) => {
+    if (context.env.DB) {
+        initializeStorage(new D1Storage(context.env.DB));
+    }
+    return handle(app)(context);
+};
--- server/storage.ts
+++ server/storage.ts
@@ -1,6 +1,11 @@
 import { MemStorage } from "./storage_mem";
+import { IStorage } from "./storage_interface";
 export * from "./storage_interface";
 
 // For Cloudflare ease-of-use, we default to MemStorage.
 // The complex DB storage can be moved to storage_db.ts if needed later.
-export const storage = new MemStorage();
+export let storage: IStorage = new MemStorage();
+
+export function initializeStorage(newStorage: IStorage) {
+    storage = newStorage;
+}
