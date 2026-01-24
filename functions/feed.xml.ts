7463529013c9fa905aaf9934a2a0ce098801c0d8
MOADSOUFIANE <joseluis45re534@gmail.com>
2026-01-24 15:32:15 +0100

    fix: update image domain in XML feed to studiazone.com

--- functions/feed.xml.ts
+++ functions/feed.xml.ts
@@ -20,19 +20,14 @@ export const onRequest = async (context: any) => {
 `;
 
         products.forEach(p => {
-            // Replace legacy studizone domain with new image host
-            const finalImageUrl = p.imageUrl.replace(
-                /https:\/\/studiazone-com-103170\.hostingersite\.com/,
-                "http://help.3dgenx.com"
-            );
-
+            const imageUrl = p.imageUrl.replace(/studiazone-com-103170\.hostingersite\.com/g, "studiazone.com");
             xml += `
 <item>
 <g:id>${p.id}</g:id>
 <g:title><![CDATA[${p.title}]]></g:title>
 <g:description><![CDATA[${p.shortDescription}]]></g:description>
 <g:link>${baseUrl}/product/${p.slug}</g:link>
-<g:image_link>${finalImageUrl}</g:image_link>
+<g:image_link>${imageUrl}</g:image_link>
 <g:condition>new</g:condition>
 <g:availability>in stock</g:availability>
 <g:price>${p.price} USD</g:price>
