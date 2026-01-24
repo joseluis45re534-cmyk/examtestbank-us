4d22cf9741bd202b7d0b02d3ee2a106a7aa28e00
MOADSOUFIANE <joseluis45re534@gmail.com>
2026-01-24 01:42:45 +0100

    fix: move feed.xml to root level for correct URL access

---
+++ functions/feed.xml.ts
@@ -0,0 +1,50 @@
+
+import { storage, initializeStorage } from '../server/storage';
+import { D1Storage } from '../server/storage_d1';
+
+export const onRequest = async (context: any) => {
+    if (context.env.DB) {
+        initializeStorage(new D1Storage(context.env.DB));
+    }
+
+    try {
+        const products = await storage.getProducts();
+        const baseUrl = "https://examtestbank-us.com";
+
+        let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss xmlns:g="http://base.google.com/ns/1.0" version="2.0">
+<channel>
+<title>Exam Test Bank US</title>
+<link>${baseUrl}</link>
+<description>Premium quality nursing, medical, and business test banks.</description>
+`;
+
+        products.forEach(p => {
+            xml += `
+<item>
+<g:id>${p.id}</g:id>
+<g:title><![CDATA[${p.title}]]></g:title>
+<g:description><![CDATA[${p.shortDescription}]]></g:description>
+<g:link>${baseUrl}/product/${p.slug}</g:link>
+<g:image_link>${p.imageUrl}</g:image_link>
+<g:condition>new</g:condition>
+<g:availability>in stock</g:availability>
+<g:price>${p.price} USD</g:price>
+<g:brand>Exam Test Bank US</g:brand>
+<g:google_product_category>6171</g:google_product_category>
+</item>`;
+        });
+
+        xml += `
+</channel>
+</rss>`;
+
+        return new Response(xml, {
+            headers: {
+                "Content-Type": "application/xml",
+            },
+        });
+    } catch (error) {
+        return new Response("Failed to generate feed", { status: 500 });
+    }
+};
