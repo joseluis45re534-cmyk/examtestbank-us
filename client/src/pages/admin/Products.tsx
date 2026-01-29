cb57979409fc540d96ebaa771e68bfcd4993c853
MOADSOUFIANE <joseluis45re534@gmail.com>
2026-01-29 16:13:11 +0100

    updat admin pannel

--- client/src/pages/admin/Products.tsx
+++ client/src/pages/admin/Products.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Product } from "@shared/schema";
 
